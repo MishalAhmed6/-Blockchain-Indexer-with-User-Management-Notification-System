3b6dd03518853afc5dfb87a75ca990e4360c5d31
MishalAhmed6 <ahmedmishal636@gmail.com>
2025-03-28 00:04:42 +0500

    Removed Unnecessary files

--- websockettest.js
+++
@@ -1,5 +0,0 @@
-const { setupBlockchainTracking } = require('./services/blockchainTracking');
-
-setupBlockchainTracking('wss://eth-mainnet.g.alchemy.com/v2/x22B2s_jwKxv_m7DwwZGducW8ak_g5pY')
-    .then(() => console.log('Tracking started'))
-    .catch(error => console.error('Setup failed', error));
